75887c83fb6c4078cb14ab4eb48d12b4732eb11c
Orion Stellaris <jonhfeel88@gmail.com>
2026-08-05 22:03:41 +0400

    fix: update registration handler and response structure

--- src/register_new.rs
+++ src/register_new.rs
@@ -1,27 +1,38 @@
 use axum::{
+    response::{IntoResponse, Response},
     extract::State,
     http::StatusCode,
     Json,
 };
 use sqlx::PgPool;
+use std::sync::Arc;
 use serde::{Deserialize, Serialize,};
+use validator::Validator;
+
+// Data Base pool (for State)
+pub struct AppConfig {
+    pub db: PgPool,
+}
 
 // Structs for register request
-#[derive(Deserialize)]
+#[derive(Deserialize, Validator)]
 pub struct RegisterRequest {
+    #[validate(lenght(min = 2, max = 55, message = "Userame must be between 2 to 55 characters long"))]
     pub username: String,
+
+    #[validate(lenght(min = 8, message = "The Password must be at least 8 characters long"))]
     pub password: String,
 }
 
 // Structs for register response
 #[derive(Serialize)]
-pub struct RegisterResponse {
+pub struct AuthResponse {
     pub message: String,
-    pub username: String,
 }
 
-// Handler for users registration StatusCode::CREATED or StatusCode::CONFLICT
-pub async fn register_handler(State(pool): State<PgPool>, Json(payload): Json<RegisterRequest>,) -> Result<(StatusCode, Json<RegisterResponse>), StatusCode> {
+pub async fn register_handler(State(ctx): State<Arc<AppConfig>>, Json(payload): Json<RegisterRequest>,) -> Result<impl IntoResponse, AppError> {
+    payload.validate().map_err(AppError::ValidationError)?;
+
     let result = sqlx::query!(
         "INSERT INTO users (username, password) VALUES ($1, $2)", 
         &payload.username, 
