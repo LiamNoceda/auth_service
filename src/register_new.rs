e7672f967847eeed40dc35ade9b29eebe3edcd6e
Orion Stellaris <jonhfeel88@gmail.com>
2026-08-05 22:28:13 +0400

    fix: improve error handling and validation messages in registration process

--- src/register_new.rs
+++ src/register_new.rs
@@ -4,33 +4,65 @@ use axum::{
     http::StatusCode,
     Json,
 };
+use argon2::{
+    password_hash::{PasswordHasher, SaltString},
+    Argon2,
+};
+use serde::{Deserialize, Serialize,};
 use sqlx::PgPool;
 use std::sync::Arc;
-use serde::{Deserialize, Serialize,};
 use validator::Validator;
 
-// Data Base pool (for State)
+// Data Base configuration struct
 pub struct AppConfig {
     pub db: PgPool,
 }
 
-// Structs for register request
+// Struct for register request
 #[derive(Deserialize, Validator)]
 pub struct RegisterRequest {
-    #[validate(lenght(min = 2, max = 55, message = "Userame must be between 2 to 55 characters long"))]
+    #[validate(length(min = 2, max = 55, message = "Username must be between 2 and 55 characters"))]
     pub username: String,
 
-    #[validate(lenght(min = 8, message = "The Password must be at least 8 characters long"))]
+    #[validate(length(min = 8, max = 130, message = "The Password must be between 8 and 130 characters"))]
     pub password: String,
 }
 
-// Structs for register response
+// Struct for auth response
 #[derive(Serialize)]
 pub struct AuthResponse {
     pub message: String,
 }
 
-pub async fn register_handler(State(ctx): State<Arc<AppConfig>>, Json(payload): Json<RegisterRequest>,) -> Result<impl IntoResponse, AppError> {
+#[derive(Serialize)]
+pub struct ErrorResponse {
+    pub error: String,
+}
+
+pub enum AppError {
+    ValidationError(String),
+    UserAlreadyExists,
+    DatabaseError(sqlx::Error),
+    InternalServerError,
+}
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        let (status, error_messge) = match self {
+            AppError::ValidationError(msg) => (StatusCode::BAD_REQUEST, msg),
+            AppError::UserAlreadyExists => (StatusCode::CONFLICT, "Username is already taken".to_string()),
+            AppError::DatabaseError(e) => {
+                eprintln!("Database error occurred: {:?}", e);
+                (StatusCode::INTERNAL_SERVER_ERROR, "Database server error".to_string())
+            }
+            AppError::InternalServerError => (StatusCode::INTERNAL_SERVER_ERROR, "Internal server error".to_string()),
+        };
+
+        (status, Json(ErrorResponse { error, error_messge})).into_response()
+    }
+}
+
+pub async fn register_handler(State(ctx): State<Arc>, Json(payload): Json<RegisterRequest>,) -> Result<impl IntoResponse, AppError> {
     payload.validate().map_err(AppError::ValidationError)?;
 
     let result = sqlx::query!(
