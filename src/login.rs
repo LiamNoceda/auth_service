e552797f7f56a6c2ff5627e1dc28054712b43d05
Orion Stellaris <jonhfeel88@gmail.com>
2026-08-07 21:46:15 +0400

    fix: add validation to LoginRequest and restructure login_handler return type

--- src/login.rs
+++ src/login.rs
@@ -2,20 +2,29 @@
 
 use axum::{
     extract::State,
+    response::{IntoResponse, Response},
     http::StatusCode,
     Json,
 };
+use argon2::{
+    password_hash::{PasswordHash, PasswordHasher, PasswordVerifier, SaltString},
+    Argon2,
+};
+use jsonwebtoken::{encode, EncodingKey, Header};
 use serde::{
     Deserialize,
     Serialize,
 };
 use sqlx::PgPool;
 use std::sync::Arc;
+use validator::Validate;
 
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Validate)]
 pub struct LoginRequest {
+    #[validate(length(min = 2, max = 55, message = "Username must be between 2 and 55 characters"))]
     pub username: String,
+    #[validate(length(min = 8, max = 130, message = "The Password must be between 8 and 130 characters"))]
     pub password: String,
 }
 
@@ -25,7 +34,12 @@ pub struct LoginResponse {
     pub token: String,
 }
 
-pub async fn login_handler(State(ctx): State<Arc<PgPool>>, Json(payload): Json<LoginRequest>) -> Result<(StatusCode, Json<LoginResponse>), StatusCode> {
+#[derive(Serialize)]
+pub struct ErrorResponse {
+    pub error: String,
+}
+
+pub async fn login_handler(State(ctx): State<Arc<PgPool>>, Json(payload): Json<LoginRequest>) -> Result<impl IntoResponse, StatusCode> {
     // 1 Валидция данных
 
     // 2 Сверка данных базы данных
