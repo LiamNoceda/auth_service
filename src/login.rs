4b7b02eb4930756587dae3bd4a0df898002746fc
Orion Stellaris <jonhfeel88@gmail.com>
2026-08-07 21:26:31 +0400

    fix: update login_handler to use PgPool for database connection

--- src/login.rs
+++ src/login.rs
@@ -25,7 +25,7 @@ pub struct LoginResponse {
     pub token: String,
 }
 
-pub async fn login_handler(State(ctx): State<Arc<AppConfig>>, Json(paylod): Json<LoginRequest>) -> Result<(StatusCode, Json<LoginResponse>), StatusCode> {
+pub async fn login_handler(State(ctx): State<Arc<PgPool>>, Json(paylod): Json<LoginRequest>) -> Result<(StatusCode, Json<LoginResponse>), StatusCode> {
     // 1 Валидция данных
 
     // 2 Сверка данных базы данных
